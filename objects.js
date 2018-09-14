c978d86d5f75eabd4773fcbfffaa57555d4b2707
Jarrod Dyer <jarrod@gasganno.com>
2018-09-14 00:53:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,13 +1,13 @@
 var playlist = { Young Widows: "Old Skin" };
 
-function updatePlaylist(object, artist, title) {
-  object[artist] = title;
-    return object
+function updatePlaylist(playlist, artistName, songTitle) {
+  playlist[artistName] = songTitle;
+    return playlist
 }
 
-function removeFromPlaylist(object, artist) {
-  delete object.artist;
-  return object
+function removeFromPlaylist(playlist, artistName) {
+  delete playlist.artistName;
+  return playlist
 }
 
 
